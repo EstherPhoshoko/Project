8a59caaa2dc4836f9e6ca5061ae7a4a3b69ead71
EstherPhoshoko <85164054+EstherPhoshoko@users.noreply.github.com>
2022-01-28 12:43:18 +0200

    Add files via upload

---
+++ project.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        int x;
+        String FirstName;
+        int y = 5;
+        String S = "Hi";
+
+        Scanner sc = new Scanner(System.in);        //Scanner statement
+
+
+        System.out.println("Enter Name");
+        FirstName = sc.nextLine();               //Read user input
+
+        System.out.println( S+" "+ FirstName);
+
+
+        x = sc.nextInt();
+
+        
+
+       
+        System.out.println(x + y);
+
+       
+
+
+        
+
+
+
+
+    
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    }
+}
